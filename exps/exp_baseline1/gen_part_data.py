67d2f1fe7306d9be1afe0e47b59ec4bb4e080ae8
Qi Li <liqi17thu@gmail.com>
2021-08-01 21:35:04 +0800

    part data

--- exps/exp_baseline1/gen_part_data.py
+++ exps/exp_baseline1/gen_part_data.py
@@ -7,7 +7,7 @@ import sapien.core as sapien
 from tqdm import tqdm
 
 from sapien_const import NO_CASUAL, SELF_CASUAL, BINARY_CASUAL
-from sapien_utils import add_model, get_pc, pc_normalize
+from sapien_utils import add_model, get_pc, get_pc_with_key, pc_normalize
 from interaction_env import gen_interaction_pc
 
 def main():
@@ -51,12 +51,13 @@ def main():
                 df.to_csv(f"part_data/{idx}.xyz")
 
             if not os.path.exists(f"part_data/{idx}_src.xyz"):
-                pc_bg = get_pc(model, keys=TRIGGER_NAME, include=False, npoints=1024)
-                pc_src = get_pc(model, keys=TRIGGER_NAME, include=True, npoints=1024)
-                pc = np.r_[pc_bg, pc_src]
-                pc = pc_normalize(pc)
-                key = np.r_[np.zeros(1024, dtype=int), np.ones(1024, dtype=int)]
+                # pc_bg = get_pc(model, keys=TRIGGER_NAME, include=False, npoints=1024)
+                # pc_src = get_pc(model, keys=TRIGGER_NAME, include=True, npoints=1024)
+                # pc = np.r_[pc_bg, pc_src]
+                # pc = pc_normalize(pc)
+                # key = np.r_[np.zeros(1024, dtype=int), np.ones(1024, dtype=int)]
                 # pc, key = gen_interaction_pc(engine, sapien_path, renderer, controller, idx, TRIGGER_NAME)
+                pc, key = get_pc_with_key(model, keys=TRIGGER_NAME, npoints=2048)
                 df = pd.DataFrame({"x": pc[:, 0], "y": pc[:, 1], "z": pc[:, 2], "key": key})
                 df.to_csv(f"part_data/{idx}_src.xyz")
 
@@ -65,10 +66,11 @@ def main():
                     pc = get_pc(model, npoints=2048)
                     key = np.ones(2048, dtype=int)
                 else:
-                    pc_bg = get_pc(model, keys=TARGET, include=False, npoints=1024)
-                    pc_dst = get_pc(model, keys=TARGET, include=True, npoints=1024)
-                    pc = np.r_[pc_bg, pc_dst]
-                    key = np.r_[np.zeros(1024, dtype=int), np.ones(1024, dtype=int)]
+                    # pc_bg = get_pc(model, keys=TARGET, include=False, npoints=1024)
+                    # pc_dst = get_pc(model, keys=TARGET, include=True, npoints=1024)
+                    # pc = np.r_[pc_bg, pc_dst]
+                    # key = np.r_[np.zeros(1024, dtype=int), np.ones(1024, dtype=int)]
+                    pc, key = get_pc_with_key(model, keys=TARGET, npoints=2048)
                 pc = pc_normalize(pc)
                 df = pd.DataFrame({"x": pc[:, 0], "y": pc[:, 1], "z": pc[:, 2], "key": key})
                 df.to_csv(f"part_data/{idx}_dst.xyz")
@@ -89,11 +91,12 @@ def main():
                 df.to_csv(f"part_data/{idx}.xyz")
 
             if not os.path.exists(f"part_data/{idx}_src.xyz"):
-                pc_bg = get_pc(model, keys=TRIGGER_NAME, include=False, npoints=1024)
-                pc_src = get_pc(model, keys=TRIGGER_NAME, include=True, npoints=1024)
-                pc = np.r_[pc_bg, pc_src]
-                pc = pc_normalize(pc)
-                key = np.r_[np.zeros(1024, dtype=int), np.ones(1024, dtype=int)]
+                # pc_bg = get_pc(model, keys=TRIGGER_NAME, include=False, npoints=1024)
+                # pc_src = get_pc(model, keys=TRIGGER_NAME, include=True, npoints=1024)
+                # pc = np.r_[pc_bg, pc_src]
+                # pc = pc_normalize(pc)
+                # key = np.r_[np.zeros(1024, dtype=int), np.ones(1024, dtype=int)]
+                pc, key = get_pc_with_key(model, keys=TRIGGER_NAME, npoints=2048)
                 # pc, key = gen_interaction_pc(engine, sapien_path, renderer, controller, idx, TRIGGER_NAME)
                 df = pd.DataFrame({"x": pc[:, 0], "y": pc[:, 1], "z": pc[:, 2], "key": key})
                 df.to_csv(f"part_data/{idx}_src.xyz")
@@ -112,10 +115,11 @@ def main():
                     pc = get_pc(model, npoints=2048)
                     key = np.ones(2048, dtype=int)
                 else:
-                    pc_bg = get_pc(model, keys=DST.responser, include=False, npoints=1024)
-                    pc_dst = get_pc(model, keys=DST.responser, include=True, npoints=1024)
-                    pc = np.r_[pc_bg, pc_dst]
-                    key = np.r_[np.zeros(1024, dtype=int), np.ones(1024, dtype=int)]
+                    # pc_bg = get_pc(model, keys=DST.responser, include=False, npoints=1024)
+                    # pc_dst = get_pc(model, keys=DST.responser, include=True, npoints=1024)
+                    # pc = np.r_[pc_bg, pc_dst]
+                    # key = np.r_[np.zeros(1024, dtype=int), np.ones(1024, dtype=int)]
+                    pc, key = get_pc_with_key(model, keys=DST.responser, npoints=2048)
                 pc = pc_normalize(pc)
                 df = pd.DataFrame({"x": pc[:, 0], "y": pc[:, 1], "z": pc[:, 2], "key": key})
                 df.to_csv(f"part_data/{idx}_dst.xyz")
